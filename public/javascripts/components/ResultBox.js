e9f3f05ef1a3ac9153e409e2d555c7919ab02a89
Chris Tweedie <cltweedie@gmail.com>
2016-04-23 15:28:07 +0100

    Display large property photo when selected

--- public/javascripts/components/ResultBox.js
+++ public/javascripts/components/ResultBox.js
@@ -3,7 +3,7 @@ window.ResultBox = React.createClass({
     var property = this.props.property;
     var photo = property.photos[0].replace('120x90', '240x180');
     return (
-      <div className="panel panel-default" onClick={this.selectProperty} >
+      <div className="panel panel-default pointer" onClick={this.selectProperty} >
         <div className="panel-body">
           <img className="col-md-5" src={"http://mr0.homeflow.co.uk/" + photo} />
           <div className="col-md-7">
--- public/javascripts/components/SelectedResult.js
+++ public/javascripts/components/SelectedResult.js
@@ -1,9 +1,15 @@
 window.SelectedResult = React.createClass({
   render: function() {
     var property = this.props.property;
+    if (property.photos) {
+      var bigPhotoSrc = property.photos[0].replace('120x90', '480x360');
+      var bigPhoto = <img src={"http://mr0.homeflow.co.uk/" + bigPhotoSrc} />
+    }
     return (
       <div>
+        { bigPhoto ? bigPhoto : ""}
         <h2>{property.price}</h2>
+        <p>{property.display_address}</p>
       </div>
     )
   }
